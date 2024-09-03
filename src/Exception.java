6a4fc1d3e4a1b33e17cda536f6d4cd70ddd42541
charlieshevick <cshevick@gmail.com>
2024-09-03 10:10:03 -0400

    try catch block

--- src/Exception.java
+++ src/Exception.java
@@ -2,9 +2,13 @@ import java.util.Scanner;
 
 public class Exception {
     public static void main(String[] args) {
-        int[] arr = new int[]{0,1,2,3,4,5};
+        int[] arr = new int[]{0, 1, 2, 3, 4, 5};
         System.out.println("Enter the index of the array to print its content");
         Scanner index = new Scanner(System.in);
-        System.out.println(arr[index.nextInt()]);
+        try {
+            System.out.println(arr[index.nextInt()]);
+        } catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Invalid Index");
+        }
     }
 }
